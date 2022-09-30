76d520452363473f4eb3b497e5028a4870d78e68
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2022-09-30 12:24:43 +0200

    Add files via upload

--- dart11.dart
+++ dart11.dart
@@ -22,39 +22,61 @@ abstract class Vehicle {
   key() => print('*bip bip*');
 }
 
-class Motorbike extends Vehicle {
+abstract class Movable {
+  void skid() {
+    print('*skids*');
+  }
+}
+
+abstract class Light {
+  void flip() {
+    print('*flips*');
+  }
+}
+
+class Motorbike extends Vehicle with Movable, Light {
   var nWheels;
   var fuel;
-
-  flip() => print('*flips*');
 }
 
-class Scooter extends Vehicle {
+class Scooter extends Vehicle with Movable, Light {
   var nWheels;
   var energy;
-
-  flip() => print('*flips*');
 }
 
-class Bike extends Vehicle {
+class Bike extends Vehicle with Movable, Light {
   var nSits;
   var energy;
-
-  flip() => print('*flips*');
 }
 
-class Car extends Vehicle {
+class Car extends Vehicle with Movable {
   var nDoors;
   var fuel;
-
-  skid() => print('*skids*');
 }
 
-class Truck extends Vehicle {
+class Truck extends Vehicle with Movable {
   var nWheels;
   var fuel;
-
-  deposit() => print('*deposits*');
 }
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  var moto = Motorbike();
+  moto.fuel = 50;
+  moto.nWheels = 2;
+  moto.code = '4567 - das';
+  moto.skid();
+  var bike = Bike();
+  bike.energy = 5;
+  bike.nSits = 1;
+  bike._driver = 'Juan';
+  bike.code = 'code_example';
+  bike.flip();
+  var car = Car();
+  car.fuel = 75;
+  car.nDoors = 6;
+  car.code = 'dfsf - 266';
+  var truck = Truck();
+  //etc
+  var scooter = Scooter();
+  //etc
+}

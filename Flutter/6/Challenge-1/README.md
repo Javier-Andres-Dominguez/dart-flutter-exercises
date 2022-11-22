888f738368e70a6d9deb82da10891a71a6c305d8
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2022-11-22 10:06:22 +0100

    Add files via upload

---
+++ Flutter/6/Challenge-1/README.md
@@ -0,0 +1,2 @@
+Make a something similar to this.
+<br>![Image](https://github.com/Javier-Andres-Dominguez/dart-flutter-exercises/blob/main/Flutter/1/image.PNG?raw=true)
---
+++ Flutter/6/Challenge-1/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Transform',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Transform class'),
+          ),
+          body: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: new EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.orange,
+                    width: 10,
+                  )),
+              width: 280,
+              height: 280,
+              alignment: Alignment.center,
+              child: Text(
+                "H",
+                style: TextStyle(
+                  fontSize: 180,
+                  color: Colors.orange,
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}

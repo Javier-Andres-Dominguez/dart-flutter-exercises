798e5d663ea9bba348f50c14de78f1639f753483
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2022-11-22 08:45:51 +0100

    Update main.dart

--- Flutter/4/lib/main.dart
+++ Flutter/4/lib/main.dart
@@ -7,20 +7,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'ejemplo de iconos',
+        title: 'Icon example',
         home: Scaffold(
             appBar: AppBar(
               title: Center(
-                child: Text('Ejemplo de Container'),
+                child: Text('Container example'),
               ),
             ),
             body: Row(children: <Widget>[
               Contenedor(Alignment.topLeft, EdgeInsets.all(12.0), Colors.cyan,
-                  120.0, 120.0, 'azul'),
+                  120.0, 120.0, 'blue'),
               Contenedor(Alignment.topRight, EdgeInsets.all(7.0), Colors.pink,
-                  100.0, 100.0, 'rosa'),
+                  100.0, 100.0, 'pink'),
               Contenedor(Alignment.center, EdgeInsets.all(20.0), Colors.green,
-                  110.0, 110.0, 'verde'),
+                  110.0, 110.0, 'green'),
             ])));
   }
 }

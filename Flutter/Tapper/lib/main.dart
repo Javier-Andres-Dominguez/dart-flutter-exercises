6b1f6d1ec93f97dffbe6faec7faa3277b774bd31
Javier Andrés Domínguez <72435329+Javier-Andres-Dominguez@users.noreply.github.com>
2023-03-06 20:06:53 +0100

    Update main.dart

--- Flutter/Tapper/lib/main.dart
+++ Flutter/Tapper/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'login_screen.dart';
 import 'game_screen.dart';
 import 'score_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  MyApp.best = prefs.getInt('bestScore') ?? 0;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static int puntuation = 0;
+  static int best = 0;
   const MyApp({super.key});
   //This is only used to be able to change windows in the app and determinate the first window
   @override
